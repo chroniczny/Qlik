4600183a234a253b8a9a1dc13b5c743be7fb5cd6
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-06-14 11:25:35 +0200

    init commit

---
+++ scripts/qdt-components.js
@@ -0,0 +1,17 @@
+// var qConfig = {
+//     "config": {
+//         "host": "https://eu-b.demo.qlik.com/QvAJAXZfc/opendoc.htm?document=qvdocs%2FHelp%20Desk%20Management.qvw&host=demo11",
+//         "secure": true,
+//         "port": 443,
+//         "prefix": "",
+//         "appId": "133dab5d-8f56-4d40-b3e0-a6b401391bde"
+//     },
+//     "connections": {
+//         "vizApi": false,
+//         "engineApi": true
+//     }
+// };
+// var vizOptions = {};
+// var QdtComponent = new window.QdtComponents(qConfig.config, qConfig.connections);
+// QdtComponent.render('QdtSelectionToolbar', {height:'300px'}, document.getElementById('qdt1'));
+// QdtComponent.render('QdtPicasso', {type: 'horizontalBar', cols: ['Case Owner Group', "Num(Avg([Case Duration Time]), '##')"], options: vizOptions, height:'300px'}, document.getElementById('qdt2'));
