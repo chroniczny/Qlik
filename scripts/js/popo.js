674352efd399cf43209880a242f7fefe538bc7db
jkwiatkowski <jkwiatkowski@pgs-soft.com>
2018-06-22 09:37:40 +0200

    Updated with comments

---
+++ scripts/js/popo.js
@@ -0,0 +1,11 @@
+
+define([], function() {
+
+
+function go() {
+    return "popo";
+}
+
+return go;
+
+});
--- scripts/start2.js
+++ scripts/start2.js
@@ -6,18 +6,22 @@
  *    Fill in host and port for Qlik engine
  */
 var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+//because Mash-up takes view from localhost:4848/start2/start2.index
+
 var config = {
-    host: window.location.hostname,
-    prefix: prefix,
-    port: window.location.port,
-    isSecure: window.location.protocol === "https:"
+    host: 'localhost',
+    prefix: '/',
+    port: '4848',
+    isSecure: false
 };
-require.config( {
+require.config( { //to take resources from QlikSense Desktop app source
     baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
 } );
 
-require( ["js/qlik"], function (qlik ) {
+require( ["js/qlik"], function ( qlik ) { //taken from QlikSense Desktop app source
+    console.log(config);
     console.log(qlik);
+
     qlik.setOnError( function ( error ) {
         $( '#popupText' ).append( error.message + "<br>" );
         $( '#popup' ).fadeIn( 1000 );
@@ -28,20 +32,21 @@ require( ["js/qlik"], function (qlik ) {
 
     //callbacks -- inserted here --
     //open apps -- inserted here --
-    var app = qlik.openApp('Consumer Sales.qvf', config);
+    var app = qlik.openApp('Consumer Sales.qvf', config); // it's a source of charts from Mash-up app
 
-    var app1 = qlik.openApp('Sales Discovery.qvf', config);
+    var app1 = qlik.openApp('Sales Discovery.qvf', config); // it's a source of charts from Mash-up app
 
     //get objects -- inserted here --
+
     app1.getObject('QV06','xvZYRNK');
-    app1.getObject('QV05','RmuRp');
+
     app1.getObject('QV04','CQgDDy');
 
     app.getObject('CurrentSelections','CurrentSelections');
 
     app.getObject('QV03','akDGX');
     app.getObject('QV02','JcJvj');
-    app.getObject('QV01','gUJWPF');
+
     //create cubes and lists -- inserted here --
 
 } );
